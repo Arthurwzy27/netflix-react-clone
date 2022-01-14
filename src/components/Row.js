515f9d2656aeb485374269ef6760d2d20f9753b4
Arthur Picard <arthur.picard7@gmail.com>
2022-01-14 19:40:32 +0100

    Add: Sass installed

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,21 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../axios';
+import './Row.scss';
 
-const Row = ({ title, fetchUrl}) => {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState ([]);
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request)
+      setMovies(request.data.results)
       return request
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]); // variable fetchUrl is outside of the block so we need to pass it here!!
+
+  console.log(movies)
 
   return (
-    <div>
-      <h2>{ movies.title }</h2>
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map(movie => (
+          <img src={`${base_url}${movie.poster_path}`} alt={movie.title} />
+        ))}
+      </div>
+
     </div>
   )
 }
