2111d4fbfb0ad5188609ffa741cd7356da4b5359
Arthur Picard <arthur.picard7@gmail.com>
2022-01-14 18:12:30 +0100

    Created baseURL for movie DB requests

---
+++ src/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// Base URLto make requests to the movie DB
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/4"
+})
+
+export default instance;
--- src/requests.js
+++ src/requests.js
@@ -1,1 +1,1 @@
-const API_KEY = "ec44a33c2b9d400ff22361b2ac030bde"
+const API_KEY = "ec44a33c2b9d400ff22361b2ac030bde";
