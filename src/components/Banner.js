2e59690e551c6911c117ac741245999fe8dda69d
Arthur Picard <arthur.picard7@gmail.com>
2022-01-15 00:15:02 +0100

    Update: Truncate function for banner__description > n

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -19,6 +19,10 @@ const Banner = () => {
   fetchData();
   }, []);
 
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n-1) + "..." : str;
+  }
+
   return (
     <header className="banner"
       style={{
