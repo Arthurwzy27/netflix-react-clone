3adc4ea8d92dc62fc745db6d1c259db123778e26
Arthur Picard <arthur.picard7@gmail.com>
2022-01-14 18:46:43 +0100

    Update: fetchURL title updated

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
 const API_KEY = "ec44a33c2b9d400ff22361b2ac030bde";
 
 const requests = {
-  fetchNetflixOriginals: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchNetflixOriginals: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 
 export default requests;
