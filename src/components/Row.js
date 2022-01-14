dff80b0062c2203786196d1a1caf256fbb632425
Arthur Picard <arthur.picard7@gmail.com>
2022-01-14 19:14:24 +0100

    Update: Hotfix useEffect with async fetchData

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import axios from './axios';
+import axios from '../axios';
 
 const Row = ({ title, fetchUrl}) => {
   const [movies, setMovies] = useState ([]);
 
   useEffect(() => {
-
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request)
+      return request
+    }
+    fetchData();
   }, []);
 
   return (
