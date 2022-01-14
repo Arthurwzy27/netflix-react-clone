d37d2829c4e62a7b11438a7aa30825d6f02f829e
Arthur Picard <arthur.picard7@gmail.com>
2022-01-14 20:12:42 +0100

    Update: Hotfix row__poster scss

--- src/components/Row.js
+++ src/components/Row.js
@@ -25,6 +25,7 @@ const Row = ({ title, fetchUrl }) => {
         {movies.map(movie => (
           <img
             key={movie.id}
+            className="row__poster"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.title} />
         ))}
