80e6f93c5f5a23d9e7c5c2e09eddb6563bda1faf
Arthur Picard <arthur.picard7@gmail.com>
2022-01-14 18:58:44 +0100

    Add: useEffect added to Row.js

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-const Row = ({title}) => {
+const Row = ({ title, fetchUrl}) => {
   const [movies, setMovies] = useState ([]);
+
+  useEffect(() => {
+
+  }, []);
 
   return (
     <div>
-      <h2>{ title }</h2>
+      <h2>{ movies.title }</h2>
     </div>
   )
 }
