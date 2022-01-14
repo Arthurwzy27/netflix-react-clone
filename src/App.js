5e8fda341b22722d2e44c01428bc1caccbd57bcf
Arthur Picard <arthur.picard7@gmail.com>
2022-01-14 20:43:09 +0100

    Add: Add all Movie Rows with fetchUrl

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import requests from './requests';
 const App = () => {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl= {requests.fetchNetflixOriginals} />
+      <Row title="Netflix Originals" fetchUrl= {requests.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl= {requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl= {requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl= {requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl= {requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl= {requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl= {requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl= {requests.fetchDocumentaries} />
     </div>
   );
 }
