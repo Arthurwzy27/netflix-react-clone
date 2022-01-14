b8ea679ea9a6e8c7f8b487b202588f46669bbd72
Arthur Picard <arthur.picard7@gmail.com>
2022-01-15 00:09:33 +0100

    Update: Banner scss title description contents

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -29,7 +29,7 @@ const Banner = () => {
       }}>
 
       <div className="banner__contents">
-        <h1>{movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">{movie?.name || movie?.original_name}</h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
