91b8135da6d83491736df6843c66eb8ea1b6d802
Arthur Picard <arthur.picard7@gmail.com>
2022-01-15 22:54:44 +0100

    Add: Search Icon in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
+import { Link } from '@mui/material';
 import React from 'react';
 import './Navbar.scss';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 const Navbar = () => {
   return (
     <div className="navbar__header">
-
         <img className="navbar__logo" src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="Netflix-logo" href="#"/>
-
       <div className="navbar__primary">
-
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/">Series</Link>
+          <Link to="/">Movies</Link>
+          <Link to="/">Trending Now</Link>
+          <Link to="/">My List</Link>
+        </li>
       </div>
       <div className="navbar__secondary">
+        <SearchIcon />
 
       </div>
     </div>
