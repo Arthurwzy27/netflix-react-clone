1a047a404f0782bf97bfecdb0e1835df28e5086c
Arthur Picard <arthur.picard7@gmail.com>
2022-01-15 23:20:35 +0100

    Update: Navbar scss diplay updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ import { Link } from '@mui/material';
 import React from 'react';
 import './Navbar.scss';
 import SearchIcon from '@mui/icons-material/Search';
+import Avatar from '@mui/material/Avatar';
+import { deepOrange } from '@mui/material/colors';
+
 
 
 const Navbar = () => {
@@ -19,6 +22,7 @@ const Navbar = () => {
       </div>
       <div className="navbar__secondary">
         <SearchIcon />
+        <Avatar sx={{ bgcolor: deepOrange[800] }} variant="square">N</Avatar>
 
       </div>
     </div>
