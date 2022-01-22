6ffcc1478927d6f1e12eb7a7ca0bd0fdb7f8c2b0
Arthur Picard <arthur.picard7@gmail.com>
2022-01-22 16:31:44 +0100

    Add: React-router-dom with Links, BrowserRouter, Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Banner from './components/Banner';
 import Navbar from './components/Navbar';
@@ -8,29 +9,30 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="navbar__banner">
-        <Navbar />
-        <Banner />
+    <BrowserRouter >
+      <div className="App">
+        <div className="navbar__banner">
+          <Navbar />
+          <Banner />
+        </div>
+        <Row
+          title="Netflix Originals"
+          fetchUrl= {requests.fetchNetflixOriginals}
+          isLargeRow={true}
+        />
+
+        <Row title="Trending Now" fetchUrl= {requests.fetchTrending} />
+        <Row title="Top Rated" fetchUrl= {requests.fetchTopRated} />
+        <Row title="Action Movies" fetchUrl= {requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl= {requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl= {requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl= {requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl= {requests.fetchDocumentaries} />
+      <div className="footer">
+        <Footer />
       </div>
-      <Row
-        title="Netflix Originals"
-        fetchUrl= {requests.fetchNetflixOriginals}
-        isLargeRow={true}
-      />
-
-      <Row title="Trending Now" fetchUrl= {requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl= {requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl= {requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl= {requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl= {requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl= {requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl= {requests.fetchDocumentaries} />
-    <div className="footer">
-      <Footer />
-    </div>
-    </div>
-
+      </div>
+    </BrowserRouter>
   );
 }
 
